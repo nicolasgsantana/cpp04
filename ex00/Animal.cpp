52ce76431cb386d773c3af37aec4c723ebb1864c
nicolasgsantana <nicolas07san@gmail.com>
2026-09-08 21:51:24 -0300

    feat: implement Animal base class

---
+++ ex00/Animal.cpp
@@ -0,0 +1,24 @@
+#include "Animal.hpp"
+
+Animal::Animal(void) : m_type("Animal")
+{
+	std::cout << "Animal default constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal &obj)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+	*this = obj;
+}
+
+Animal &Animal::operator=(const Animal &obj)
+{
+	if (this != &obj)
+		this->m_type = obj.m_type;
+	return (*this);
+}
+
+Animal::~Animal(void)
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
---
+++ ex00/Animal.hpp
@@ -0,0 +1,19 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <string>
+# include <iostream>
+
+class Animal
+{
+protected:
+	std::string	m_type;
+
+public:
+	Animal(void);
+	Animal(const Animal &obj);
+	Animal &operator=(const Animal &obj);
+	~Animal(void);
+};
+
+#endif
