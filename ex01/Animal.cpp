0b4845ade133d3c55ad7a182b4ef4d0ecc31b0e9
nicolasgsantana <nicolas07san@gmail.com>
2026-09-15 10:39:00 -0300

    fix Animal to not be abstract anymore

--- ex01/Animal.cpp
+++ ex01/Animal.cpp
@@ -32,3 +32,8 @@ void Animal::makeSound(void) const
 {
 	std::cout << this->m_type << " says: Unspecified sound" << std::endl;
 }
+
+void Animal::checkBrain(void) const
+{
+	std::cout << "I don't have a brain" << std::endl;
+}
